e83caaf7b0a3d5ace105367e1b1cb524d52bbbfc
zemla4ok <dima-vital@live.com>
2018-03-18 22:16:14 +0300

    crud controller

--- controllers/crud.js
+++ controllers/crud.js
@@ -16,9 +16,54 @@ class CrudController{
             '/': [{method: 'get', cb: this.readAll}],
             '/:id': [{method: 'get', cb: this.read}],
             '/': [{method: 'post', cb: this.create}],
-            '/': [{method: 'put', cb: this.update}],
-            '/': [{method: 'delete', cb: this.delete}]
-        }
+            '/:id': [{method: 'put', cb: this.update}],
+            '/:id': [{method: 'delete', cb: this.delete}]
+        };
+    }
+
+    async readAll(req, res){
+        let data = await this.service.readChunk(req.query);
+        res.json(data);
+    }
+
+    async read(req, res){
+        let data = await this.service.read(req.params.id);
+        this.cache.set(req, data);
+        res.json(data); 
+    }
+
+    async create(req, res) {
+        res.json(
+            await this.service.create(req.body)
+          );          
+    }
+
+    async update(req, res) {
+        res.json(
+            await this.service.update(req.params.id, req.body)
+          );          
+    }
+
+    async delete(req, res) {
+        res.json(
+            await this.service.delete(req.params.id)
+          );
+    }
+
+    registerRoutes() {
+        Object.keys(this.routes).forEach(route => {
+            let handlers = this.routes[route];
+            if(!handlers || !Array.isArray(handlers)){
+                return;
+            }
+
+            for(let handler of handlers) {
+                this.router[handler.method](
+                    route,
+                    wrap(handler.cb)
+                );
+            }
+        });
     }
 }
 
