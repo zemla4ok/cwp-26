637e864414c51eb2ed1ff1ce6a9f1f05e0b5e105
zemla4ok <dima-vital@live.com>
2018-03-19 17:30:52 +0300

    validation endpoint for user

--- services/user.js
+++ services/user.js
@@ -4,6 +4,17 @@ class UserService extends CrudService{
     constructor(repository){
         super(repository);
     }
+
+    async validate(userId, validationToken){
+        let user = await this.read(userId);
+        if (user.validationToken === validationToken){
+            user.update({validated: true});
+            return {status: 200, msg: 'OK'};
+        }
+        else{
+            return {status: 400, msg: 'token error'};
+        }
+    }
 }
 
-module.exports = UserService;
+module.exports = UserService; 
